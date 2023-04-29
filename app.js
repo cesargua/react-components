04272c7e7f42f62360c7b7ad5166febcfbd59db2
cesargua <cguajard@ucsd.edu>
2023-04-28 21:10:56 -0700

    from 3 weeks ago but just in case

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // TODO
+const { useState } = React;
+const GroceryListItem = (props) => {
+    const [isDone, setIsDone] = useState(false);
+    //const {isHovered, setIsHovered} = useState(false); 
+    const style = {
+        fontWeight: isDone ? 'bold' : 'normal',
+    };
+
+    return (
+    <li style={style} onMouseEnter={() => { 
+        return setIsDone(true)
+    }} 
+    onMouseLeave={() => { return setIsDone(false);}} 
+    >{props.item} </li>)
+}
+
+const GroceryList = (props) => {
+    // const onClickGroceryItem = (event) =>{
+    //     console.log('clicked!')
+    // }
+   
+    return (
+    <ul>
+        {props.items.map((item) => (
+            <GroceryListItem item={item}/>
+        ))}
+    </ul>);
+};
+
+const App = () => (
+    <div>
+        <h2>My Grocery List</h2>
+        <GroceryList items={['Eggs', 'Milk', 'EggPlant', 'Cucumber', 'kale']}/>
+    </div>
+);
+// <GroceryList GroceryListItem={['Eggs', 'Milk', 'EggPlant', 'Cucumber', 'kale']}/> 
+
+ReactDOM.render(<App />, document.getElementById("app"));
